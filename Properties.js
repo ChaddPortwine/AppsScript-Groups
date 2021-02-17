828fd1723a365076d17cef77aeedd60a459df335
Chadd Portwine <cportwine@jahnelgroup.com>
2021-02-17 17:36:58 -0500

    Add Properties get/set

--- Properties.js
+++ Properties.js
@@ -1,3 +1,23 @@
-function myFunction() {
-  
-}
+/*
+ * Use these functions to set and get secrets
+ * from the Apps Script PropertiesService
+ * 
+ * Properties to set
+ * "domain" = ''
+ * "sheetName" = ''
+ */
+
+// RUN: clasp run setProperty -p '["key","value"]'
+function setProperty(key,value) {
+    let scriptProperties = PropertiesService.getScriptProperties()
+    scriptProperties.setProperty(key,value)
+    return "setProperty Complete"
+};
+function logProperties() {
+    let scriptProperties = PropertiesService.getScriptProperties()
+    return scriptProperties
+};
+function getProperty(key){
+    let scriptProperties = PropertiesService.getScriptProperties()
+    return scriptProperties.getProperty(key)
+};
